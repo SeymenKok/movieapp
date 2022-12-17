88f7eca7ae8d294eb02fb963ea5eefb113adaddc
seymen <seymenkok0630@gmail.com>
2022-12-17 23:36:49 +0300

    inital commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Main from './Components/Main';
+import './Components/style.css';
+import Card from './Components/Card.js';
+
+function App() {
+  return (
+    <>  
+    <Main/>
+    </>
+  );
+}
+
+export default App;
+/*<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>*/
